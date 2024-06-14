952490dd1e18c0a6fbc07537bac859bd2831134e
bamadhu <madhuba07@gmail.com>
2024-06-14 01:06:21 -0400

    Initial z/OS port

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/ZOSOpenTools/libsasl2port/actions/workflows/bump.yml/badge.svg)](https://github.com/ZOSOpenTools/libsasl2port/actions/workflows/bump.yml)
+
+# libsasl2
+
+his is the Cyrus SASL API implementation. It can be used on the client or server side to provide authentication and authorization services. See RFC 4422 for more information.
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/libsasl2port.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libsasl2port.git'), string(name: 'PORT_DESCRIPTION', value: 'his is the Cyrus SASL API implementation. It can be used on the client or server side to provide authentication and authorization services. See RFC 4422 for more information.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
