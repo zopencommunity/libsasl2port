514b345e740b42b3b8911f7db83f6f7dfe33cb42
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:54 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/libsasl2port.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/libsasl2port.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libsasl2port.git'), string(name: 'PORT_DESCRIPTION', value: 'his is the Cyrus SASL API implementation. It can be used on the client or server side to provide authentication and authorization services. See RFC 4422 for more information.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libsasl2port.git'), string(name: 'PORT_DESCRIPTION', value: 'his is the Cyrus SASL API implementation. It can be used on the client or server side to provide authentication and authorization services. See RFC 4422 for more information.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
